bd38631e9a7f0ffcc996a90c867b78df372d0b59
Alice Brunel <alc.brunel@gmail.com>
2019-07-02 08:54:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(array)
     move(array, index, "X")
     display_board
   else
-    until valid_movevalid_move?(array,index) == true
+    until valid_move?(array,index) == true
     turn(array)
   end
 end
