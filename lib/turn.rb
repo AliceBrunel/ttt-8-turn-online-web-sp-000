8ef31db6d47e43fe56452fea21216229556f9caa
Alice Brunel <alc.brunel@gmail.com>
2019-07-02 08:22:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,20 @@
-
-
-def valid_move?(board, index)
-
-  index.between?(0,8) && !position_taken?(board, index)
+def display_board(array)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
 
-def position_taken?(board, index)
-  board[index] != " "
+def valid_move?(array, index)
+
+  index.between?(0,8) && !position_taken?(array, index)
 end
+
+def position_taken?(array, index)
+  array[index] != " "
+end
+
+def move(array, index, "X") {
+  
+}
